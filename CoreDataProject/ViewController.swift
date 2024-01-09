26c452151cbb52a1be1c73d17ca73f5d219317c3
Abishek joseph <abishek@coveiot.com>
2024-01-09 21:38:32 +0530

    Initial Commit

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Jiva Ram on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
