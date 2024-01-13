f5705f4e751cce4009eff4e9eaf49e38f1425c8a
Abishek joseph <abishek@coveiot.com>
2024-01-13 12:21:48 +0530

    coredat save and retreive

--- CoreDataProject/ViewController.swift
+++ CoreDataProject/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -13,5 +14,59 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello core data...")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        createData()
+    }
 }
 
+extension ViewController {
+    
+    func createData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+                   print("Error: AppDelegate is nil")
+                   return
+               }
+
+               let manageContext = appDelegate.persistentContainer.viewContext
+
+               guard let userEntity = NSEntityDescription.entity(forEntityName: "UserEntity", in: manageContext) else {
+                   print("Error: User entity not found")
+                   return
+               }
+        
+        for i in 1...5 {
+            let user = NSManagedObject(entity: userEntity, insertInto: manageContext)
+            user.setValue("Jiva Ram \(i)", forKeyPath: "username")
+            user.setValue("jivanbh.com \(i)", forKey: "email")
+            user.setValue("jiva \(i)", forKey: "password")
+        }
+        
+        do{
+            try manageContext.save()
+            print("values set successfully..")
+            retrieveData()
+        } catch let error as NSError{
+            print("could not save \(error), \(error.userInfo)")
+        }
+    }
+    
+    func retrieveData() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            print("Error: AppDelegate is nil")
+            return
+        }
+        let manageContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "UserEntity")
+        
+        do{
+            let result = try manageContext.fetch(fetchRequest)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "username") as! String)
+            }
+        } catch {
+            print("fetching faild...")
+        }
+    }
+}
